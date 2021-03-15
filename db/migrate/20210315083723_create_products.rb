847dfdcf35c881e5d97c827e7b6986293bfdafe4
krystalmm <taeyeon_aina@live.jp>
2021-03-15 08:44:22 +0000

    [add] genres table

--- db/migrate/20210315083723_create_products.rb
+++ db/migrate/20210315083723_create_products.rb
@@ -1,11 +1,11 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-      t.integer :genre_id
-      t.string :name
-      t.text :description
-      t.integer :price
-      t.boolean :is_valid
+      t.integer :genre_id, null: false
+      t.string :name, null: false
+      t.text :description, null: false
+      t.integer :price, null: false
+      t.boolean :is_valid, null: false, default: true
       t.string :product_image_id
       
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_083723) do
+ActiveRecord::Schema.define(version: 2021_03_15_084122) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,6 +52,12 @@ ActiveRecord::Schema.define(version: 2021_03_15_083723) do
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
 
+  create_table "genres", force: :cascade do |t|
+    t.string "genre", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "order_products", force: :cascade do |t|
     t.integer "order_id", null: false
     t.integer "product_id", null: false
@@ -76,11 +82,11 @@ ActiveRecord::Schema.define(version: 2021_03_15_083723) do
   end
 
   create_table "products", force: :cascade do |t|
-    t.integer "genre_id"
-    t.string "name"
-    t.text "description"
-    t.integer "price"
-    t.boolean "is_valid"
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.boolean "is_valid", default: true, null: false
     t.string "product_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
