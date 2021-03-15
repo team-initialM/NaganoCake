792661e611225752e148651a42b94811f8cd0805
krystalmm <taeyeon_aina@live.jp>
2021-03-15 08:13:45 +0000

    [add] ShippingAddresses model

---
+++ db/migrate/20210315081111_create_shipping_addresses.rb
@@ -0,0 +1,12 @@
+class CreateShippingAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shipping_addresses do |t|
+      t.integer :customer_id, null: false
+      t.string :postcode, null: false
+      t.string :address, null: false
+      t.string :address_name, null: false
+
+      t.timestamps
+    end
+  end
+end
