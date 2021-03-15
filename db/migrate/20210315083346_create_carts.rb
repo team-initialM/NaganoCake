341fc3ee1acd48108b9f17127620a7fdaed39adc
krystalmm <taeyeon_aina@live.jp>
2021-03-15 08:37:08 +0000

    [add] carts table

--- db/migrate/20210315083346_create_carts.rb
+++ db/migrate/20210315083346_create_carts.rb
@@ -1,8 +1,13 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-
+      t.integer :product_id, null: false
+      t.integer :customer_id, null: false
+      t.integer :quantity, null: false
+      
       t.timestamps
     end
+    add_foreign_key :carts, :products
+    add_foreign_key :carts, :customers
   end
 end
