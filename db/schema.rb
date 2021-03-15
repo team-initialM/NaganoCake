4e702b82aab4172ac3cb59075ff8ef82bc30def1
krystalmm <taeyeon_aina@live.jp>
2021-03-15 08:41:06 +0000

    [add] products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_083346) do
+ActiveRecord::Schema.define(version: 2021_03_15_083723) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -71,6 +71,17 @@ ActiveRecord::Schema.define(version: 2021_03_15_083346) do
     t.string "address_name", null: false
     t.integer "payment_selection", default: 0, null: false
     t.integer "postage", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.integer "genre_id"
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.boolean "is_valid"
+    t.string "product_image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
