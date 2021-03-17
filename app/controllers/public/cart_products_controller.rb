355d2ada52b41e100c06668a89e7e4fa4837b0df
MOEKASAKAI <wpi.floves2@gmail.com>
2021-03-17 06:59:36 +0000

    [Update] Cart products controller and index view

--- app/controllers/public/cart_products_controller.rb
+++ app/controllers/public/cart_products_controller.rb
@@ -1,16 +1,38 @@
 class Public::CartProductsController < ApplicationController
   def index
+    @cart_products = CartProduct.where(customer_id: current_customer.id)
   end
 
   def create
+    @cart_product = current_user.cart_products.create(cart_products_params)
+    @cart_product.save
+    redirect_to cart_products_path
   end
   
   def update
+    @cart_product = CartProduct.find(params[:id])
+    if @cart_product.update(cart_product_params)
+      redirect_to cart_products_path
+      flash[:notice] = "The quantity of selected items has changed."
+    end
   end
 
   def destroy
+    @cart_product = CartProduct.find(params[:id])
+    @cart_product.destroy
+    redirect_to cart_products_path
+    flash[:notice] = "The selected item has been removed from the cart."
   end
   
   def destroy_all
+    @cart_products = Cart.where(customer_id: current_customer.id)
+    @cart_products.destroy
+    redirect_to cart_products_path
+    flash[:notice] = "All selected items has been removed from the cart."
   end
+  
+  private
+    def cart_products_params
+      params.require(:cart_product).permit(:quantity)
+    end
 end
