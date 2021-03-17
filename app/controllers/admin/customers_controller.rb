60d129853a096112ab62d74b103ac89d1cb17096
akasatan <aka435304@gmail.com>
2021-03-17 06:55:59 +0000

    [add]admin_customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,28 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    else
+      render index
+    end
   end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:firstname, :lastname, :kana_firstname, :kana_lastname, :postcode, :address, :phone_number, :email, :is_valid)
+  end
+
 end
