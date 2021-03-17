574b1c2248633684889b0b0cda3bcacb0fe8d2a7
akasatan <aka435304@gmail.com>
2021-03-17 14:13:18 +0000

    [add]public_orders.new.confirm.show

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -10,16 +10,46 @@ class Public::OrdersController < ApplicationController
       @product_total += order_product.product_price * order_product.quantity
     end
   end
-  
+
   def new
+    @addresses = current_customer.shipping_addresses
+    @cart_products = Cart.where(user_id: current_user.id)
+    @order = Order.new
   end
-  
+
   def create
+    @cart = Cart.where(customer_id: current_customer.id)
+    @order = Order.new(order_params)
+    if @order.save
+      @cart.destroy_all
+      redirect_to complete_orders_path
+    else
+      render :new
+    end
   end
-  
+
   def confirm
+    @order = Order.new(order_params)
+    @carts = Cart.where(customer_id: current_customer.id)
+    if params[:order][:address_option] == "0"
+      @order.postcode = current_customer.postcode
+    elsif params[:order][:address_option] == "1"
+      @order.postcode = current_customer.postcode
+    elsif params[:order][:address_option] == "2"
+      @order.postcode = params[:order][:postcode]
+      @order.address = params[:order][:address]
+      @order.address_name = params[:order][:address_name]
+    end
+    @order.order_products.each do |order_product|
+      @product_total += order_product.product_price * order_product.quantity
+    end
   end
 
   def complete
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:customer_id, :order_status, :total_price, :postcode, :address, :address_name, :payment_selection, :postage)
   end
 end
