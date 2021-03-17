c9aa0fa567ecf925bb08d28235ed9a5d1eed93f3
krystalmm <taeyeon_aina@live.jp>
2021-03-17 16:34:29 +0900

    [add] create products and show product page

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,12 +3,20 @@ class Admin::ProductsController < ApplicationController
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def update
   end
 
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = 'products created successfully.'
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
   end
 
   def new
@@ -16,5 +24,11 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:genre_id, :name, :description, :price, :is_valid, :product_image)
   end
 end
