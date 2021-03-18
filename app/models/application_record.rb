d335fa852e787796595883c3a9bfe5d721a3fe45
krystalmm <taeyeon_aina@live.jp>
2021-03-18 17:57:19 +0900

    [update] order created at and include helpermethod to application_record model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
+  include ActionView::Helpers::ApplicationHelper
+
   self.abstract_class = true
-  def include_tax(price)
-    tax = 1.1
-    ((price * tax).round(2)).ceil
-  end
 end
