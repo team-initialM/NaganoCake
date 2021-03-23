91ca4e159a8d9ac12e9460947da58f0f1c120ef0
krystalmm <taeyeon_aina@live.jp>
2021-03-23 12:02:35 +0900

    [add] customer model spec

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -6,4 +6,59 @@ RSpec.describe Customer, type: :model do
   it 'is valid factory' do
     expect(customer).to be_valid
   end
+
+  it 'is invalid without a firstname' do
+    customer.firstname = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without a lastname' do
+    customer.lastname = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without a kana_firstname' do
+    customer.kana_firstname = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without a kana_lastname' do
+    customer.kana_lastname = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without an email' do
+    customer.email = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid with a duplicate email address' do
+    other_customer = FactoryBot.build(:customer, email: customer.email)
+    other_customer.valid?
+    expect(other_customer.errors[:email]).to include("has already been taken")
+  end
+
+  it 'is invalid without a password' do
+    customer.password = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without a postcode' do
+    customer.postcode = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without an address' do
+    customer.address = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is invalid without a phone_number' do
+    customer.phone_number = ""
+    expect(customer).to be_invalid
+  end
+
+  it 'is true is_valid with a default customer' do
+    expect(customer.is_valid).to eq true
+  end
 end
