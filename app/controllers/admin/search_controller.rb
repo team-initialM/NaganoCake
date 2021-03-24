993f743b4f0c65b0506765b95bde881bfb1061bb
MOEKASAKAI <wpi.floves2@gmail.com>
2021-03-24 05:10:53 +0000

    [Update] search controller to more search items

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -2,10 +2,14 @@ class Admin::SearchController < ApplicationController
   def index
 
     @range = params[:range]
-    if @range == "Customer"
+
+    case @range
+    when "会員情報"
       @customers = Customer.looks(params[:search], params[:word]).page(params[:page]).per(10)
-    else
+    when "商品名"
       @products = Product.looks(params[:search], params[:word]).page(params[:page]).per(10)
+    when "注文者名"
+      @orders = Order.looks(params[:search], params[:word]).page(params[:page]).per(10)
     end
   end
 end
