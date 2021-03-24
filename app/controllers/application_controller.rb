e499d7a8a28f012a0a2159b2a8e46fa0706cf516
krystalmm <taeyeon_aina@live.jp>
2021-03-24 14:09:56 +0900

    [add] cart_product ajax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   include ApplicationHelper
 
+  after_action :discard_flash_if_xhr
+
   protected
   def after_sign_in_path_for(resource)
     case resource
@@ -23,5 +25,9 @@ class ApplicationController < ActionController::Base
     else
       root_path
     end
+  end
+
+  def discard_flash_if_xhr
+    flash.discard if request.xhr?
   end
 end
